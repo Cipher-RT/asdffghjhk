418fc98ff159d756e913faee592ebed2d8e46539
mulka <munkhtulga.b2015@orkhonschool.edu.mn>
2026-09-22 13:15:53 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,9 +1,20 @@
 # You can remove 'pass' if you written code in the function
 # Exercise 1
 
-def find_closest(data, target):
-    # Write your code here
-    pass
+def find_insert_position(data, target):
+
+    low = 0
+
+    high = len(data) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if data[mid] < target:
+            low = mid + 1
+        else:
+            high = mid
+    if data[len(data)-1]<target:
+        low+=1
+    return data[low-1]
 
 # Exercise 2
 def integer_sqrt(n):
